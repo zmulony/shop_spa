d5628921a381019a57898b6542ec1ab2454d8ded
zmulony <sefferynka@ubuntu.(none)>
2012-11-11 16:52:44 +0100

    created new rails app; added some vendor files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopSpa::Application.config.secret_token = '0b35c45abff27c951fd89b20e85828dafe36fa332c85d4a1b251d5c40318932b36d0677874b3e58598ee598241b0b4d4ae3790fd0c74d829479d414214e7fc94'
